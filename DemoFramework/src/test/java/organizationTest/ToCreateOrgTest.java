46feb8416650c924da9fe1ba1c12842b285c75f8
rajbalaji003 <rajbalaji003@gmail.com>
2024-11-05 08:50:09 +0530

    First Push

---
+++ DemoFramework/src/test/java/organizationTest/ToCreateOrgTest.java
@@ -0,0 +1,11 @@
+package organizationTest;
+
+public class ToCreateOrgTest {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("organization created successfully");
+	}
+
+}
